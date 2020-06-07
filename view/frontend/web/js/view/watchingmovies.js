489757a7458644a9c7aa007545cbaf30435a69be
nicolasblancom <nicolasblancom@gmail.com>
2020-06-07 18:09:13 +0200

    Modify switchstatus functionality to use knockout native foreach binding instead of repeat binding, using a duplicate of movies array

--- view/frontend/web/js/view/watchingmovies.js
+++ view/frontend/web/js/view/watchingmovies.js
@@ -1,5 +1,7 @@
-define(['uiComponent', 'jquery'], function(Component, $){
+define(['uiComponent', 'jquery', 'ko'], function(Component, $, ko){
     'use strict';
+
+    var self;
 
     return Component.extend({
         defaults: {
@@ -8,17 +10,24 @@ define(['uiComponent', 'jquery'], function(Component, $){
                 {id: 2, label: "Movie name 2", status: false},
                 {id: 3, label: "Movie name 3", status: false},
                 {id: 4, label: "Movie name 4", status: true}
+            ],
+            movies2: [
+                ko.observable({id: 1, label: "Movie name 1", status: false}),
+                ko.observable({id: 2, label: "Movie name 2", status: false}),
+                ko.observable({id: 3, label: "Movie name 3", status: false}),
+                ko.observable({id: 4, label: "Movie name 4", status: true})
             ]
         },
 
         initialize: function(){
-            var self = this;
+            self = this;
             this._super();
         },
 
         initObservable: function(){
             this._super().observe([
-                'movies'
+                'movies',
+                'movies2'
             ]);
 
             return this;
@@ -37,6 +46,22 @@ define(['uiComponent', 'jquery'], function(Component, $){
             //console.log(items);
 
             this.movies(items);
+        },
+        switchStatus2: function (data, event) {
+            const movieId = $(event.target).data('id');
+
+            console.log(self.movies2());
+
+            let items = self.movies2().map(function(_movie){
+                if (_movie().id === movieId) {
+
+                    _movie().status = !_movie().status;
+                }
+                console.log(_movie());
+                return ko.observable(_movie());
+            });
+
+            self.movies2(items);
         }
     })
 });
