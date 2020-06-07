ced25760e4e2d0b4d5e76d37229bb742f2c9971e
nicolasblancom <nicolasblancom@gmail.com>
2020-06-07 16:52:28 +0200

    Modify loop (knockout repeat binding does some context magic), add switch status functionality

--- view/frontend/web/js/view/watchingmovies.js
+++ view/frontend/web/js/view/watchingmovies.js
@@ -1,14 +1,19 @@
-define(['uiComponent'], function(Component){
+define(['uiComponent', 'jquery'], function(Component, $){
     'use strict';
 
     return Component.extend({
         defaults: {
             movies: [
-                {label: "Movie name 1", status: false},
-                {label: "Movie name 2", status: false},
-                {label: "Movie name 3", status: false},
-                {label: "Movie name 4", status: true}
+                {id: 1, label: "Movie name 1", status: false},
+                {id: 2, label: "Movie name 2", status: false},
+                {id: 3, label: "Movie name 3", status: false},
+                {id: 4, label: "Movie name 4", status: true}
             ]
+        },
+
+        initialize: function(){
+            var self = this;
+            this._super();
         },
 
         initObservable: function(){
@@ -17,6 +22,21 @@ define(['uiComponent'], function(Component){
             ]);
 
             return this;
+        },
+
+        switchStatus: function (data, event) {
+            const movieId = $(event.target).data('id');
+
+            let items = this.movies().map(function(_movie){
+                if (_movie.id === movieId) {
+                    _movie.status = !_movie.status;
+                }
+                return _movie;
+            });
+
+            //console.log(items);
+
+            this.movies(items);
         }
     })
 });
