cae0a8403473abcb9cd97a045d5b7bb305911b6d
nicolasblancom <nicolasblancom@gmail.com>
2020-06-09 17:38:56 +0200

    delete console log

--- view/frontend/web/js/view/watchingmovies.js
+++ view/frontend/web/js/view/watchingmovies.js
@@ -43,21 +43,17 @@ define(['uiComponent', 'jquery', 'ko'], function(Component, $, ko){
                 return _movie;
             });
 
-            //console.log(items);
-
             this.movies(items);
         },
         switchStatus2: function (data, event) {
             const movieId = $(event.target).data('id');
-
-            console.log(self.movies2());
 
             let items = self.movies2().map(function(_movie){
                 if (_movie().id === movieId) {
 
                     _movie().status = !_movie().status;
                 }
-                console.log(_movie());
+
                 return ko.observable(_movie());
             });
 
